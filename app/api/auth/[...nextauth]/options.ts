64336d65b26f9971410076af4552756d09a63c25
Tony Piña <pina.rossette@comunidad.unam.mx>
2023-09-21 07:20:16 -0600

    Primer protoripo de login

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,35 @@
+import type  { NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+export const options: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "Usuario:",
+                    type: "text",
+                    placeholder: "tu-usuario"
+                },
+                password: {
+                    label: "Contraseña:",
+                    type: "password"
+                }
+            },
+            async authorize(credentials, req) {
+                // Aquí tengo que traer la información del usuario
+                const user = {
+                    id: "42",
+                    name: "Prueba",
+                    password: "prueba"
+                }
+
+                if (credentials?.username === user.name && credentials?.password === user.password) {
+                    return user
+                }
+
+                return null
+            }
+        })
+    ],
+}
