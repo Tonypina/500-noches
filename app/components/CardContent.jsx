c0ef9e873cf17cf535d8d6acef301e130b614b30
Tony Piña <65735766+Tonypina@users.noreply.github.com>
2024-08-08 17:18:17 -0600

    Se agregó el texto de "Próximamente" en la carta de Histórico

--- app/components/CardContent.jsx
+++ app/components/CardContent.jsx
@@ -6,14 +6,21 @@ const CardContent = ({ logo, imagen, id }) => {
       <div className="bg-white h-full px-0 pt-0 max-h-screen flex flex-col justify-between items-center py-10">
         <div className="mt-0 mx-0 mb-auto w-full">{imagen}</div>
         <div className="my-auto">{logo}</div>
-        <Boton
-          storeId={id}
-          type={"tienda"}
-          className="mt-auto"
-          variante={"bordered"}
-          contenido={"Descubrir"}
-          style={"bg-white text-main"}
-        />
+        {id === "historico-cafe-tostador" ? 
+          <>
+            <p>Próximamente</p>
+            <p className="scale-[0.6]">San Cristóbal de las Casas</p>
+          </>
+        : 
+          <Boton
+            storeId={id}
+            type={"tienda"}
+            className="mt-auto"
+            variante={"bordered"}
+            contenido={"Descubrir"}
+            style={"bg-white text-main"}
+          />
+        }
       </div>
     </>
   );
